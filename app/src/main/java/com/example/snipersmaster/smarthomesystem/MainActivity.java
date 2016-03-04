93243f91bba6e34025e1e5e8233974a07d599129
SnipersMaster <snipersmaster@gmail.com>
2016-03-04 23:43:00 +0400

    friday work

--- app/src/main/java/com/example/snipersmaster/smarthomesystem/MainActivity.java
+++ app/src/main/java/com/example/snipersmaster/smarthomesystem/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.snipersmaster.smarthomesystem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +21,11 @@ import org.json.JSONObject;
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
-TextView tc,tm,ts,td;
+
+    Switch d1,d2,d3,d4;
+    TextView tc,tm,ts,td;
     AsyncHttpClient client;
-    String url = "http://192.168.0.91:3000/avgdailytemp";
+    String url = "http://192.168.0.91:3000/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,13 +34,50 @@ TextView tc,tm,ts,td;
         tc=(TextView)findViewById(R.id.txtCurrentTemp);
         tm=(TextView)findViewById(R.id.txtMonthlyTemp);
         ts=(TextView)findViewById(R.id.txtSeasonallyTemp);
+        d1=(Switch)findViewById(R.id.device1);
+        d2=(Switch)findViewById(R.id.device2);
+        d3=(Switch)findViewById(R.id.device3);
+        d4=(Switch)findViewById(R.id.device4);
+        d1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){Change("1","D1","1");}
+                else if(!isChecked){Change("1","D1","0");}
+            }
+        });
+        d2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){Change("1","D2","1");}
+                else if(!isChecked){Change("1","D2","0");}
+            }
+        });
+        d3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){Change("1","D3","1");}
+                else if(!isChecked){Change("1","D3","0");}
+            }
+        });
+        d4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){Change("1","D4","1");}
+                else if(!isChecked){Change("1","D4","0");}
+            }
+        });
+
+
         avgdaily();
+        avgmonthly();
+        avgseasonlly();
+        current();
     }
     void avgdaily(){
         RequestParams params = new RequestParams();
         params.put("user","1");
         client = new AsyncHttpClient();
-        client.post(url, params,new JsonHttpResponseHandler() {
+        client.post(url+"avgDailytemp", params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 super.onSuccess(statusCode, headers, response);
@@ -47,16 +88,110 @@ TextView tc,tm,ts,td;
                     e.printStackTrace();
                 }
             }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Toast.makeText(MainActivity.this, responseString, Toast.LENGTH_SHORT).show();
+                throwable.printStackTrace();
+            }
         });
 
     }
     void avgmonthly(){
-
+        RequestParams params = new RequestParams();
+        params.put("user","1");
+        client = new AsyncHttpClient();
+        client.post(url+"avgMonthlyTemp", params,new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONObject obj = response.getJSONObject(0);
+                    tm.setText(obj.getString("AVG"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Toast.makeText(MainActivity.this, responseString, Toast.LENGTH_SHORT).show();
+                throwable.printStackTrace();
+            }
+        });
     }
     void avgseasonlly(){
+        RequestParams params = new RequestParams();
+        params.put("user","1");
+        client = new AsyncHttpClient();
+        client.post(url+"avgSeason", params,new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONObject obj = response.getJSONObject(0);
+                    ts.setText(obj.getString("AVG"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
 
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Toast.makeText(MainActivity.this, responseString, Toast.LENGTH_SHORT).show();
+                throwable.printStackTrace();
+            }
+
+        });
     }
     void current(){
+        RequestParams params = new RequestParams();
+        params.put("user","1");
+        client = new AsyncHttpClient();
+        client.post(url+"currentTemp", params,new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONObject obj = response.getJSONObject(0);
+                    tc.setText(obj.getString("Temp"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Toast.makeText(MainActivity.this, responseString, Toast.LENGTH_SHORT).show();
+                throwable.printStackTrace();
+            }
+        });
+    }
+    void Change(String user,String name,String mode){
+        RequestParams params = new RequestParams();
+        params.put("user",user);
+        params.put("DN",name);
+        params.put("DM",mode);
+        client = new AsyncHttpClient();
+        client.post(url+"changeDeviceMode", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONObject obj = response.getJSONObject(0);
+                    Toast.makeText(MainActivity.this, obj.getString("status"), Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Toast.makeText(MainActivity.this, responseString, Toast.LENGTH_SHORT).show();
+                throwable.printStackTrace();
+            }
+        });
 
     }
 }
