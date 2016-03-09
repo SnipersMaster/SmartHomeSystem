fa2034685c1e8560010f993699241e1d0719497d
SnipersMaster <snipersmaster@gmail.com>
2016-03-09 22:37:38 +0400

    wed work - full

---
+++ app/src/main/java/com/example/snipersmaster/smarthomesystem/Scanner.java
@@ -0,0 +1,61 @@
+package com.example.snipersmaster.smarthomesystem;
+
+import android.app.Activity;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+
+public class Scanner extends Activity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    private TextView myTextView;
+    private QRCodeReaderView mydecoderview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scanner);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+    }
+
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        finish();
+        //Toast.makeText(Scanner.this, text, Toast.LENGTH_SHORT).show();
+        app.Set(text);
+    }
+
+
+    // Called when your device have no camera
+    @Override
+    public void cameraNotFound() {
+
+    }
+
+    // Called when there's no QR codes in the camera preview image
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.getCameraManager().startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.getCameraManager().stopPreview();
+    }
+}
